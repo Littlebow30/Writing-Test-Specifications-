9a4c8c06a137038f38fd94e573319bb16532dc63
Littlebow30 <bowmanA1906@gmail.com>
2023-09-18 11:48:45 -0400

    try again

---
+++ test.utils.js
@@ -0,0 +1,13 @@
+// Multipication
+
+// create a funtion that takes two numbers and multiplys them
+// the output should be the multiplication of both numbers
+// this should be able to take both positive and negitve numbers 
+// if only one number is given then we should multiply the number by itself.
+
+// Concats Odds
+
+// This fuction should take an multiple arrays of numbers 
+// This fuction should take out the odd numbers and place them in another array.
+// if the number is already in the array, we should not add it 
+//we should expect the numbers to be lesast to greatest. 
